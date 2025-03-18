55267e64800bafb3a6aa56f90770eee989903234
ducnguyenhuy <ducnguyenhuy2000@gmail.com>
2025-03-18 22:18:50 +0700

    Add the implentation of combo 1 intermediate

--- 1_combo_intermediate.cpp
+++ 1_combo_intermediate.cpp
@@ -6,4 +6,54 @@
  *      The Shape objects should be stored and managed using std::unique_ptr
  */
 
- 
+#include <iostream>
+#include <memory>
+
+#define PI 3.14
+
+class Shape
+{
+public:
+    virtual double area(void) = 0;
+    virtual ~Shape() {}             // add a virtual destructor
+};
+
+class Rectangle : public Shape
+{
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(double in_width, double in_height) : width(in_width), height(in_height) {}
+    ~Rectangle() {}
+
+    double area(void) override
+    {
+        return width * height;
+    }
+};
+
+class Circle : public Shape
+{
+private:
+    double radius;
+
+public:
+    Circle(double in_radius) : radius(in_radius) {}
+    ~Circle(void) {}
+
+    double area(void) override
+    {
+        return radius * radius * PI;
+    }
+};
+
+int main(void)
+{
+    std::unique_ptr<Shape> shape_obj = std::make_unique<Circle>(12.5);
+
+    std::cout << "The area: " << shape_obj->area() << std::endl;
+
+    return 0;
+}
